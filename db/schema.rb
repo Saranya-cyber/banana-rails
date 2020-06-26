a6a06305b61196404ee203fbec7a7bc3e2e2c2c3
Pravalika Routhu <pravalikarouthu@Pravalikas-MacBook-Air.local>
2020-06-26 16:16:54 -0700

    Added the new passwords for seeds data.

--- db/schema.rb
+++ db/schema.rb
@@ -87,8 +87,8 @@ ActiveRecord::Schema.define(version: 2020_05_28_234911) do
     t.string "business_license"
     t.string "account_status"
     t.string "pickup_instructions"
-    t.decimal "latitude", precision: 10, scale: 6
-    t.decimal "longitude", precision: 10, scale: 6
+    t.decimal "latitude"
+    t.decimal "longitude"
     t.string "first_name"
     t.string "last_name"
     t.string "phone_number"
@@ -106,74 +106,6 @@ ActiveRecord::Schema.define(version: 2020_05_28_234911) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["resettable_type", "resettable_id"], name: "index_password_resets_on_resettable_type_and_resettable_id"
-  end
-
-  create_table "rpush_apps", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "environment"
-    t.text "certificate"
-    t.string "password"
-    t.integer "connections", default: 1, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "type", null: false
-    t.string "auth_key"
-    t.string "client_id"
-    t.string "client_secret"
-    t.string "access_token"
-    t.datetime "access_token_expiration"
-    t.text "apn_key"
-    t.string "apn_key_id"
-    t.string "team_id"
-    t.string "bundle_id"
-    t.boolean "feedback_enabled", default: true
-  end
-
-  create_table "rpush_feedback", force: :cascade do |t|
-    t.string "device_token"
-    t.datetime "failed_at", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "app_id"
-    t.index ["device_token"], name: "index_rpush_feedback_on_device_token"
-  end
-
-  create_table "rpush_notifications", force: :cascade do |t|
-    t.integer "badge"
-    t.string "device_token"
-    t.string "sound"
-    t.text "alert"
-    t.text "data"
-    t.integer "expiry", default: 86400
-    t.boolean "delivered", default: false, null: false
-    t.datetime "delivered_at"
-    t.boolean "failed", default: false, null: false
-    t.datetime "failed_at"
-    t.integer "error_code"
-    t.text "error_description"
-    t.datetime "deliver_after"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.boolean "alert_is_json", default: false, null: false
-    t.string "type", null: false
-    t.string "collapse_key"
-    t.boolean "delay_while_idle", default: false, null: false
-    t.text "registration_ids"
-    t.integer "app_id", null: false
-    t.integer "retries", default: 0
-    t.string "uri"
-    t.datetime "fail_after"
-    t.boolean "processing", default: false, null: false
-    t.integer "priority"
-    t.text "url_args"
-    t.string "category"
-    t.boolean "content_available", default: false, null: false
-    t.text "notification"
-    t.boolean "mutable_content", default: false, null: false
-    t.string "external_device_id"
-    t.string "thread_id"
-    t.boolean "dry_run", default: false, null: false
-    t.index ["delivered", "failed", "processing", "deliver_after", "created_at"], name: "index_rpush_notifications_multi", where: "((NOT delivered) AND (NOT failed))"
   end
 
 end
