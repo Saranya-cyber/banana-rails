e3329e70333d0041d22b04b93fce55865b5e1465
Drew Hite <drew@drewhite.org>
2020-06-29 15:13:44 -0400

    PR suggested changes for migrating default value from 'pending' to 'processing'

--- test/controllers/clients_controller_test.rb
+++ test/controllers/clients_controller_test.rb
@@ -5,16 +5,24 @@ class ClientsControllerTest < ActionDispatch::IntegrationTest
 
   test "we return 409 status code in the event the client email is already present in the db" do
     post clients_create_url, params: {client: { email: "client@client.com", password: "does not matter",
-                                                   address_zip: 90210, account_status: "pending"}}
+                                                   address_zip: 90210}}
     assert_response :conflict
   end
 
   test "we successfully register a new client" do
     post clients_create_url, params: {client: { email: "notindb@notindb.com", password: "password1!",
-                                                address_zip: 90210, account_status: "pending", first_name: "Newname", last_name: "Client"}}
+                                                address_zip: 90210, first_name: "Newname", last_name: "Client"}}
     assert_response :success
     just_added = Client.find_by_email("notindb@notindb.com")
     assert_not_nil just_added
+  end
+
+  test "we successfully register a new client and account status defaults to processing" do
+    post clients_create_url, params: {client: { email: "acc_status_notindb@notindb.com", password: "password1!",
+                                                address_zip: 90210, first_name: "Newname", last_name: "Client"}}
+    assert_response :success
+    just_added = Client.find_by_email("acc_status_notindb@notindb.com")
+    assert_equal AccountStatus::PROCESSING, just_added.account_status, "account_status should have defaulted to #{AccountStatus::PROCESSING}"
   end
 
 end
--- test/controllers/donors_controller_test.rb
+++ test/controllers/donors_controller_test.rb
@@ -5,19 +5,30 @@ class DonorsControllerTest < ActionDispatch::IntegrationTest
 
   test "we return 409 status code in the event the donor email is already present in the db" do
     post donors_create_url, params: {donor: { email: "donor@donor.com", password: "does not matter",
-                                                   address_zip: 90210, account_status: "pending"}}
+                                                   address_zip: 90210 }}
     assert_response :conflict
   end
 
   test "we successfully register a new donor" do
     post donors_create_url, params: {donor: { email: "notindb@notindb.com", password: "password1!", first_name: "New",
-                                              address_zip: 98104, account_status: "pending", last_name: "Donor",
+                                              address_zip: 98104, account_status: AccountStatus::PROCESSING, last_name: "Donor",
                                               organization_name: "Uwajimaya", address_street: "600 5th Ave S",
                                               pickup_instructions: "On the back steps",
                                               address_city: "Seattle", address_state: "WA", business_license: "ADASDFG"}}
     assert_response :success
     just_added = Donor.find_by_email("notindb@notindb.com")
     assert_not_nil just_added
+  end
+
+  test "we register a new donor and account_status defaults to processing" do
+    post donors_create_url, params: {donor: { email: "acc_status_test@notindb.com", password: "password1!", first_name: "New",
+                                              address_zip: 98104, last_name: "Donor",
+                                              organization_name: "Uwajimaya", address_street: "600 5th Ave S",
+                                              pickup_instructions: "On the back steps",
+                                              address_city: "Seattle", address_state: "WA", business_license: "ADASDFG"}}
+    assert_response :success
+    just_added = Donor.find_by_email("acc_status_test@notindb.com")
+    assert_equal AccountStatus::PROCESSING, just_added.account_status, "account_status should default to #{AccountStatus::PROCESSING} if not specified"
   end
 
 end
