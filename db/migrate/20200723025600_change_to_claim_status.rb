908f61870265e24d8a305374ca3ef312fe567ea3
Drew Hite <drew@drewhite.org>
2020-07-23 21:35:08 -0400

    remove time claimed column

--- db/migrate/20200723025600_change_to_claim_status.rb
+++ db/migrate/20200723025600_change_to_claim_status.rb
@@ -2,6 +2,7 @@ class ChangeToClaimStatus < ActiveRecord::Migration[6.0]
   def change
     remove_column :claims, :completed
     remove_column :claims, :canceled
+    remove_column :claims, :time_claimed
     add_column :claims, :status, :string
   end
 end
